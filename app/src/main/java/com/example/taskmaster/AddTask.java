ab3958488fc1bcbc460e3f8f589489c79972f19d
JamesDansie <jdansie8336@gmail.com>
2019-10-22 11:17:21 -0700

    added submit notification

--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -3,6 +3,8 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddTask extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AddTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        Button submitTask = findViewById(R.id.AddTask);
+        submitTask.setOnClickListener((event) -> {
+            findViewById(R.id.submitText).setVisibility(View.VISIBLE);
+        });
     }
 }
