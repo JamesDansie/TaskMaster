1e95be874cfeaf267d9da97d4961b569e9d27942
JamesDansie <jdansie8336@gmail.com>
2019-10-24 10:54:42 -0700

    functional recycle view

---
+++ app/src/main/java/com/example/taskmaster/TaskAdapter.java
@@ -0,0 +1,70 @@
+package com.example.taskmaster;
+
+import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+    private List<Task> tasks;
+    private OnTaskInteractionListener listener;
+
+    public TaskAdapter(List<Task> tasks, OnTaskInteractionListener listener) {
+        this.tasks = tasks;
+        this.listener = listener;
+    }
+
+    public static class TaskViewHolder extends RecyclerView.ViewHolder {
+        Task task;
+        TextView taskTitleView;
+        TextView taskDescriptionView;
+        public TaskViewHolder(@NonNull View taskView) {
+            super(taskView);
+            this.taskTitleView = taskView.findViewById(R.id.TaskTitle);
+            this.taskDescriptionView = taskView.findViewById(R.id.TaskDescription);
+        }
+    }
+
+
+
+    // RecyclerView needs us to create a brand new row, from scratch, for holding data
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_task, parent, false);
+        final TaskViewHolder holder = new TaskViewHolder(v);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.potato(holder.task);
+            }
+        });
+        return holder;
+    }
+
+    // RecyclerView has a row (maybe previously used?) that needs to be updated for a particular location/index
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        Task taskAtPosition = this.tasks.get(position);
+        holder.task = taskAtPosition;
+        holder.taskTitleView.setText(taskAtPosition.getTitle());
+        holder.taskDescriptionView.setText(taskAtPosition.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.tasks.size();
+    }
+
+    // Make sure that my adapter can communicate with any Activity it's a part of that implements this interface
+    public static interface OnTaskInteractionListener {
+        public void potato(Task task);
+    }
+
+}
