a74f68f28c63026a4009018998755ad75eab45b4
JamesDansie <jdansie8336@gmail.com>
2019-10-29 11:09:32 -0700

    made a get request, now to parse it

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -8,12 +8,27 @@ import androidx.room.Room;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTaskInteractionListener{
     private List<Task> tasks;
@@ -66,6 +81,14 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTas
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://taskmaster-api.herokuapp.com/tasks")
+                .build();
+
+        client.newCall(request).enqueue(new LogTasksCallback(this));
+
         //****************** Buttons ****************
 
         Button addTaskButton = findViewById(R.id.ButtonTaskAdd);
@@ -96,5 +119,44 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTas
         goToDetailIntent.putExtra("taskDescription", task.getDescription());
 
         MainActivity.this.startActivity(goToDetailIntent);
+    }
+
+    public void putDataOnPage(String data){
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(data);
+
+        Gson gson = new Gson();
+        GsonArr incomingArr = gson.fromJson(data, InternetTask.class);
+    }
+}
+
+class LogTasksCallback implements Callback {
+    MainActivity currentMainActivityInstance;
+
+    public LogTasksCallback(MainActivity currentMainActivityInstance){
+        this.currentMainActivityInstance = currentMainActivityInstance;
+    }
+
+    private static final String TAG = "Callback";
+
+    @Override
+    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+        Log.e(TAG, "internet error");
+        Log.e(TAG, e.getMessage());
+    }
+
+    @Override
+    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+        String responseBody = response.body().string();
+        Log.i(TAG, responseBody);
+
+        Handler handlerForMainThread = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message inputMessageToMain){
+                currentMainActivityInstance.putDataOnPage((String)inputMessageToMain.obj);
+            }
+        };
+        Message completeMessage = handlerForMainThread.obtainMessage(0, responseBody);
+        completeMessage.sendToTarget();
     }
 }
